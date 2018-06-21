f18dae386279c58ec45d79d0cfb84f1c78750936
ANITHAS22 <39801902+ANITHAS22@users.noreply.github.com>
2018-06-21 22:39:16 +0530

    Palindrome.java

---
+++ Palindrome.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Palindrome
+{
+    public static void main(String args[])
+    {
+        int n,m,a=0,x;
+        Scanner s=new Scanner(System.in);
+        n=s.nextInt();
+        m=n;
+        while(n>0)
+        {
+            x=n%10;
+            a=a*10+x;
+            n=n/10;
+        }
+        if(a==m)
+        {
+            System.out.println("yes");
+        }
+        else
+        {
+            System.out.println("no");
+        }
+    }
+}
